c19728f9f490ec1a19ec405e136a85f62ce30f3c
guillen-jorge88 <guillen.jorge88@gmail.com>
2021-04-28 22:26:30 -0500

    create bottomsheet

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,14 +1,17 @@
-import '../tabs/tabs.dart';
 import 'package:flutter/material.dart';
+import '../tabs/tabs.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   Color textColor = Color.fromRGBO(33, 33, 33, 1.0);
-  List<Widget> tabsList = [
+  int _currentIndex = 0;
+
+  List<Tab> tabsList = [
     Tab(
       icon: Icon(Icons.home),
       text: "Home",
@@ -17,9 +20,13 @@ class _HomePageState extends State<HomePage> {
       icon: Icon(Icons.explore),
       text: "Explore",
     ),
-    // Tab(
-    //   icon: Icon(Icons.notifications),
-    // ),
+    Tab(
+      icon: Icon(
+        Icons.add_circle_outline,
+        size: 48.0,
+      ),
+      text: '',
+    ),
     Tab(
       icon: Icon(Icons.subscriptions),
       text: "Subscriptions",
@@ -29,13 +36,29 @@ class _HomePageState extends State<HomePage> {
       text: "Library",
     )
   ];
+
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: tabsList.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4,
+      length: tabsList.length,
       child: Scaffold(
+        backgroundColor: Color.fromRGBO(249, 249, 249, 1.0),
         appBar: AppBar(
-          elevation: 0.0,
+          elevation: 1.5,
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
@@ -73,31 +96,87 @@ class _HomePageState extends State<HomePage> {
               )
             ],
           ),
-          backgroundColor: Color(0xFFFFFF),
-          bottom: PreferredSize(
-              child: Container(
-                color: Color.fromRGBO(33, 33, 33, 0.2),
-                height: 1.0,
-              ),
-              preferredSize: Size.fromHeight(4.0)),
+          backgroundColor: Color.fromRGBO(255, 255, 255, 1.0),
+          // bottom: PreferredSize(
+          //   child: Container(
+          //     color: Color.fromRGBO(33, 33, 33, 0.2),
+          //     height: 1.0,
+          //   ),
+          //   preferredSize: Size.fromHeight(4.0),
+          // ),
         ),
         body: TabBarView(
-          children: <Widget>[
+          controller: _tabController,
+          children: [
             HomeTab(),
             ExploreTab(),
+            Container(),
             SubscriptionsTab(),
-            LibraryTab()
+            LibraryTab(),
           ],
         ),
-        bottomNavigationBar: TabBar(
-          labelStyle: TextStyle(fontSize: 10, fontFamily: "Helvetica"),
-          tabs: tabsList,
-          labelColor: textColor,
-          unselectedLabelColor: Color(0xFF909090),
-          indicatorColor: Colors.transparent,
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0.0, 1.0),
+                blurRadius: 2.0,
+              ),
+            ],
+          ),
+          child: TabBar(
+            controller: _tabController,
+            labelStyle: TextStyle(fontSize: 10, fontFamily: "Helvetica"),
+            tabs: tabsList,
+            labelColor: textColor,
+            unselectedLabelColor: Color(0xFF909090),
+            indicatorColor: Colors.transparent,
+            onTap: (value) => _onItemTapped(value, context),
+          ),
         ),
-        // backgroundColor: Color(0xFF282828),
       ),
+    );
+  }
+
+  void _onItemTapped(int index, BuildContext context) {
+    (index == 2) ? showSheet(context) : _currentIndex = index;
+    print('index:::::: $index');
+  }
+
+  void showSheet(BuildContext context) {
+    _tabController?.index = _currentIndex;
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return Container(
+          color: Color(0xff757575),
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
+              ),
+            ),
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                    leading: Icon(Icons.upload),
+                    title: Text('Upload a video'),
+                    onTap: () => {}),
+                ListTile(
+                  leading: Icon(Icons.wifi_tethering),
+                  title: Text('Stream a video'),
+                  onTap: () => {},
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
